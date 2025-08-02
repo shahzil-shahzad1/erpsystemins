a2c59158f96cb05a4f88026a2d4b7a206a0ea5f6
Shahzil Shahzad <silentboys1117@gmail.com>
2025-08-02 17:23:32 +0500

    Update page.tsx

--- src/app/(dashboard)/hr/attendance/page.tsx
+++ src/app/(dashboard)/hr/attendance/page.tsx
@@ -72,8 +72,8 @@ const EmployeeAttendancePage: React.FC = () => {
         <p className="text-zinc-800 dark:text-zinc-200 text-xs">Track employee presence, absence, and leave</p>
         </div>
         <div className="flex gap-3 items-center">
-        <a href="/hr/attendance/overview" className="bg-white/50 dark:bg-gray-800/50 text-black dark:text-white">Overview</a>
-        <a href="/hr/attendance/overview" className="bg-white/50 dark:bg-gray-800/50 text-black dark:text-white">Management</a>
+        <a href="/hr/attendance/overview" className="bg-white/50 dark:bg-gray-800/50 text-black dark:text-white py-1.5 px-3 rounded-lg shadow-md">Overview</a>
+        <a href="/hr/attendance/overview" className="bg-white/50 dark:bg-gray-800/50 text-black dark:text-white py-1.5 px-3 rounded-lg shadow-md">Management</a>
         </div>
       </div>
 
